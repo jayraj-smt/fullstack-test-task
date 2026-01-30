2c5fd76daa3c6363dac95f189e21c6ce9c718774
Jayraj Kapadia <jayraj@secretmindtech.com>
2026-01-30 18:15:00 +0530

    package changes

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -1,6 +1,17 @@
 import { Sequelize } from 'sequelize'
 import config from './index.js'
 
+const dialectOptions = {}
+
+if (config.db.ssl) {
+  dialectOptions.ssl = {
+    require: true,
+    // allow self-signed certs if the deploy does not provide a CA bundle
+    rejectUnauthorized: !!config.db.sslRejectUnauthorized,
+  }
+  if (config.db.sslCa) dialectOptions.ssl.ca = config.db.sslCa
+}
+
 const sequelize = new Sequelize(
   config.db.database,
   config.db.username,
@@ -16,6 +27,9 @@ const sequelize = new Sequelize(
       acquire: 30000,
       idle: 10000,
     },
+    dialectOptions: Object.keys(dialectOptions).length
+      ? dialectOptions
+      : undefined,
   },
 )
 
